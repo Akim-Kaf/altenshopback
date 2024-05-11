fa376f2db4bbe0345412f5a78d5ea6b201b2e181
Akim-Kaf <akimkafanga@gmail.com>
2024-05-11 12:35:47 +0200

    init commit

---
+++ src/main/java/com/altern/altenshopback/AltenshopbackApplication.java
@@ -0,0 +1,13 @@
+package com.altern.altenshopback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltenshopbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltenshopbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/altern/altenshopback/AltenshopbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altern.altenshopback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltenshopbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
